a2557dedc19b8fcbd01c1682be632b0c42c314c0
Filip Studený <BluesyPompanno@seznam.cz>
2022-05-29 17:41:16 +0200

    bla

---
+++ init.py
@@ -0,0 +1,10 @@
+###############################
+#         MAC CHANGER         #
+###############################
+#!/usr/bin/env python3
+
+import subprocess as ps
+
+ps.call("ipconfig", shell=True)
+
+
